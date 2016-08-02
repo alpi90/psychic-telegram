277d3e0eb16104cb16ec0ee0ede3dbce52da3f03
Alp Kaya <alpkaya@hotmail.com.tr>
2016-08-02 14:19:15 +0200

    server response

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ app.get('/', function (req, res) {
 // Facebook Webhook
 app.get('/webhook', function (req, res) {
     if (req.query['hub.verify_token'] === 'testbot_verify_token') {
-        res.send(req.query['hub.challenge']);
+        res.send("12345");
     } else {
         res.send('Invalid verify token');
     }
